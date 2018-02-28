7ea7100ba9444e1db66cdd2c4c5057fa1054bc8d
gandlurisk <skumar@pfchoice.com>
2018-02-28 15:42:53 -0500

    modified for Rolespecification

--- src/main/java/com/pfchoice/springboot/repositories/specifications/RoleSpecifications.java
+++ src/main/java/com/pfchoice/springboot/repositories/specifications/RoleSpecifications.java
@@ -23,8 +23,8 @@ public class RoleSpecifications implements Specification<Role> {
 		String containsLikePattern = getContainsLikePattern(searchTerm);
 		
 		Predicate p = cb.conjunction();
-		//p.getExpressions().add(cb.or(cb.like(cb.lower(root.get("role")), containsLikePattern)));
-		//p.getExpressions().add(cb.and(cb.equal(root.get("activeInd"), 'Y')));
+		p.getExpressions().add(cb.or(cb.like(cb.lower(root.get("role")), containsLikePattern)));
+		p.getExpressions().add(cb.and(cb.equal(root.get("activeInd"), 'Y')));
 		cq.distinct(true);
 		return p;
 		
